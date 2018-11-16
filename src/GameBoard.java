df91f04aa4c041f5c77b976896f56965fa8dc8f7
nikolajwassmann <nwassmann@hotmail.com>
2018-11-16 10:52:10 +0100

    Lavet Controller klasse, samt rettet i GUI

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,9 +1,38 @@
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
 import gui_main.GUI;
 
-public class GameBoard {
-    public static void main(String[] arg){
-        GUI gui = new GUI();
-        
-    }
+import java.awt.*;
 
+public class GameBoard {
+    public static void main(String[] args) {
+        GUI_Field[] fields = new GUI_Field[2];
+
+
+
+
+        //Dette er så simpelt som det kan blive.
+        GUI_Street testStreet1= new GUI_Street();
+        testStreet1.setTitle("Anker Engelundsvej");
+        testStreet1.setBorder(Color.CYAN); //Useful to show owner
+        testStreet1.setRent("600,-");
+        testStreet1.setSubText("");
+        fields[0] = testStreet1;
+
+
+
+        //Der tilføjes et par værdier.
+
+        GUI_Street testStreet= new GUI_Street();
+        testStreet.setTitle("Anker Engelundsvej");
+        testStreet.setBorder(Color.CYAN); //Useful to show owner
+        testStreet.setRent("600,-");
+        testStreet.setSubText("");
+        fields[1] = testStreet;
+
+
+        GUI gui = new GUI(fields,Color.BLACK);
+
+
+    }
 }
