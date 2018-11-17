a8ecd10554035c50fa6c5620e36f9d2b81f22fcc
s185114 <s185114@student.dtu.dk>
2018-11-17 02:41:36 +0100

    Hurtig skitse af spilleren.

--- src/Players.java
+++ src/Players.java
@@ -1,2 +1,27 @@
 public class Players {
+    private String name;
+    private int color;
+    private int balance;
+    private boolean win;
+
+    public void Spiller() {
+        balance=0;
+        win=false;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(int balance) {
+        if (balance < 0) {
+            this.balance = 0;
+        } else
+            this.balance = balance;
+    }
+
+    /**
+     * bare lige en hurtig skitse, hvis det skal være, kan banken også flyttes ud til sin egen klasse, hvor spilleren så kan oprette en indstans af banken, men dette ville nok ikke være specielt mere GFRASP end det her.
+     */
+
 }
