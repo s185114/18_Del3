3d958df3ce72cc4d275e2e5f68e256cd6fbf7aca
s185114 <s185114@student.dtu.dk>
2018-11-28 00:35:48 +0100

    Tilføjede en inputklasse, eftersom det burde opfylde GRASP bedre.

---
+++ src/Input.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Input {
+    private java.util.Scanner Scanner = new Scanner(System.in);
+    private String kast = "";
+
+    //private DiceCup dice = new DiceCup(); todo det er måske smartere at have selve kastet i den klasse, hvor vi opbygger, hvad en tur indebærer.
+
+    private boolean wrongin() {
+        boolean wrong = false;
+        if (!kast.equals("2")) {  //tjekker for korrekt input
+            System.out.println("Forkert input...");
+            wrong = true;
+        }
+        return wrong;
+    }
+
+    private void throwDice() {       //todo forstår stadig ikke helt, om vi burde lave en get-metode til denne type funktioner, eller om den bare skal være public, eftersom der jo ikke er nogle variabler med.
+        do {
+            System.out.println("Tast 2 for at kaste terningen:");
+            kast = Scanner.nextLine();
+        } while (wrongin());
+        //dice.getcup();
+    }
+
+
+}
