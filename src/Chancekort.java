0111d6e895580c1f65b734e4f09528d10b9f7a50
nikolajwassmann <nwassmann@hotmail.com>
2018-11-29 21:31:59 +0100

    Har lavet alle chancekort

---
+++ src/Chancekort.java
@@ -0,0 +1,41 @@
+public class Chancekort {
+
+    private String[] kort = new String[14];
+    {
+        kort[0] = "Ryk frem til start og modtag 400";
+
+        kort[1] = "Ryk 5 felter frem";
+
+        kort[2] = "Ryk frem til det nærmeste orange felt. Hvis det er ledigt, får du det GRATIS! Ellers skal du betale husleje";
+
+        kort[3] = "Ryk et felt frem eller tag et chancekort mere";
+
+        kort[4] = "Du har spist for meget slik. Betal 200";
+
+        kort[5] = "Ryk frem til det nærmeste orange eller grønne felt. Hvis det er ledigt, får du det GRATIS! Ellers skal du betale husleje";
+
+        kort[6] = "Ryk frem til et gråt felt. Hvis det er ledigt, får du det GRATIS! Ellers skal du betale husleje";
+
+        kort[7] = "Du løslades uden omkostning. Behold dette kort indtil du får brug for det";
+
+        kort[8] = "Ryk frem til Strandpromenaden";
+
+        kort[9] = "Det er din fødselsdag. Alle giver dig 200. Tillykke med fødselsdagen";
+
+        kort[10] = "Ryk frem til det nærmeste pink eller blåt felt. Hvis det er ledigt, får du det GRATIS! Ellers skal du betale husleje";
+
+        kort[11] = "Du har lavet alle dine lektier. Modtag 400 fra banken";
+
+        kort[12] = "Ryk frem til det nærmeste røde felt. Hvis det er ledigt, får du det GRATIS! Ellers skal du betale husleje";
+
+        kort[13] = "Ryk frem til Skateparken for at lave fedetricks. Hvis det er ledigt, får du det GRATIS! Ellers skal du betale husleje";
+    }
+
+    public String[] getKort() {
+        return kort;
+    }
+
+    public void setKort(String[] kort) {
+        this.kort = kort;
+    }
+}
