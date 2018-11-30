ff5c0f26565bd6cab47b74e9eb7dea50b5a1b1e5
s185114 <s185114@student.dtu.dk>
2018-11-30 16:50:41 +0100

    Jumnit af træk

--- src/ChancekortTest.java
+++ src/ChancekortTest.java
@@ -4,6 +4,15 @@ public class ChancekortTest {
 
     @org.junit.Test
     public void trækkort() {
+        //Vores trækkort trækker et kort, samt sørger for, at det er et nyt kort, man trækker næste gang.
+        Chancekort chance = new Chancekort();
+        String old = "";
+        for (int i = 0; i < 100; i++) {
+            String ny = chance.trækkort();
+            System.out.println(ny);
+            assertTrue(!old.equals(ny));
+            old = ny;
+        }
     }
 
     @org.junit.Test
